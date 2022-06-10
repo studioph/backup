1e3feb1058ba6366cde30239a72e7058f8fd7dea
Paul Hutchings <36493057+studioph@users.noreply.github.com>
2022-06-09 20:26:50 -0400

    Added restic prune

--- backup-dev/backup.sh
+++ backup-dev/backup.sh
@@ -6,4 +6,5 @@ IFS=$'\n\t'
 BACKUP_SOURCE="$1"
 
 restic backup "$BACKUP_SOURCE" --exclude-file "$BACKUP_SOURCE/.config/backup/restic.exclude" --one-file-system
+restic forget --prune --keep-daily 7
 rclone sync "$RESTIC_REPOSITORY" "$RCLONE_REMOTE"
